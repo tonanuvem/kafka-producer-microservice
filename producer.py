498ef19c9f74a6f4feb569f5b7375865a258fca8
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-02-02 22:11:35 -0200

    Update producer.py

--- producer.py
+++ producer.py
@@ -7,14 +7,12 @@ def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
 def create(msg):
-    """
-    This function creates a new person in the people structure
-    based on the passed in person data
-    :param person:  person to create in people structure
-    :return:        201 on success, 406 on person exists
-    """
+   
     texto = msg.get("texto", None)
-
+    topico = "meu-topico"
+    broker = "192.168.10.133:9092"
+    
+    """
     # Tentando enviar a msg pro Kafka?
     try:
         # Create an instance of the Kafka producer
@@ -50,4 +48,57 @@ def create(msg):
             406,
             "Erro 2 ao enviara msg pro Kafka: "+str(e),
         )
+        """
+    # --------
+    # USAGE: https://kafka-python.readthedocs.io/en/master/usage.html
+    producer = KafkaProducer(bootstrap_servers=[broker])
 
+    # Asynchronous by default
+    future = producer.send(topico, b'raw_bytes')
+
+    # Block for 'synchronous' sends
+    try:
+        record_metadata = future.get(timeout=10)
+    except KafkaError:
+        # Decide what to do if produce request failed...
+        log.exception()
+        pass
+
+    # Successful result returns assigned partition and offset
+    print (record_metadata.topic)
+    print (record_metadata.partition)
+    print (record_metadata.offset)
+
+    # produce keyed messages to enable hashed partitioning
+    producer.send(topico, key=b'foo', value=b'bar')
+
+    # encode objects via msgpack
+    producer = KafkaProducer(value_serializer=msgpack.dumps)
+    producer.send(topico, {'key': 'value'})
+
+    # produce json messages
+    producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('ascii'))
+    producer.send(topico, {'key': 'value'})
+
+    # produce asynchronously
+    for _ in range(100):
+        producer.send(topico, b'msg')
+
+    # produce asynchronously with callbacks
+    producer.send(topico, b'raw_bytes').add_callback(on_send_success).add_errback(on_send_error)
+
+    # block until all async messages are sent
+    producer.flush()
+
+    # configure multiple retries
+    producer = KafkaProducer(retries=5)
+
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+def on_send_error(excp):
+    log.error('I am an errback', exc_info=excp)
+    # handle exception
+    pass
